1220e832ef0472d12b146a19e30e3127b8e7b88e
hhjkhhbgkugjnbj <ck8901ck@gmail.com>
2024-11-24 17:50:19 +0900

    modify2

--- chatBot_server/src/main/java/com/capstone/chatBotServer/App/Controller/chatBotController.java
+++ chatBot_server/src/main/java/com/capstone/chatBotServer/App/Controller/chatBotController.java
@@ -1,13 +1,11 @@
 package com.capstone.chatBotServer.App.Controller;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
